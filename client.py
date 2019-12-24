088f9c63ff17be297cd2797b92c9b995d906953a
Alexandra Kolomenskaya <avkolomenskaya@gmail.com>
2019-12-24 21:48:25 +0300

    основное задание

--- client.py
+++ client.py
@@ -1,14 +1,56 @@
 import socket
 import pickle
 
+def chis(g,l,p):
+    return g**l%p
+
+def crypt(msg, k):
+    crypt_msg=''
+    for i in range(len(msg)):
+        crypt_msg+=chr(ord(msg[i])^k)
+    return crypt_msg
+
+def send_msg(conn, msg, k):
+	sock.send(pickle.dumps(crypt(msg, k)))
+
+def recv_msg(conn, k):
+	msg=crypt(pickle.loads(sock.recv(1024)), k)
+	return msg
+
 HOST = '127.0.0.1'
-PORT = 8080
+try:
+    PORT=int(input("ваш порт:"))
+    if not 0 <= PORT <= 65535:
+        raise ValueError
+except ValueError :
+    PORT = int(input('Введите еще раз:'))
 
 sock = socket.socket()
+
+
 sock.connect((HOST, PORT))
+print('Соединение установлено')
+print('Для отсоединения введите "disconnect"')	
+
 
 p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
+Aa =  chis(g,a,p)
+sock.send(pickle.dumps((p, g, Aa)))
+
+B = pickle.loads(sock.recv(1024))
+
+K = chis(B,a,p)
+print('K=',K)
+
+msg = input('Отправляем на сервер: ')
+while True: 
+	try:
+		while msg!='disconnect':
+			send_msg(sock, msg, K)
+			print(' ' + recv_msg(sock, K))
+			msg=input('> ')
+		break
+	except:
+		break
 
 sock.close()
--- server.py
+++ server.py
@@ -1,15 +1,70 @@
 import socket
 import pickle
+from random import randint
+
+def random_free_port(p):
+	try:
+		sock = socket.socket()
+		sock.bind(('',p))
+		sock.close
+		return p
+	except:
+		for _ in range(0, 65536):
+			try:
+				p=randint(0,65535)
+				sock = socket.socket()
+				sock.bind(('',p))
+				sock.close
+				return p
+			except:
+				continue    
+
+def chis(g,l,p):
+    return g**l%p
+
+def crypt(msg, k):
+    crypt_msg=''
+    for i in range(len(msg)):
+        crypt_msg+=chr(ord(msg[i])^k)
+    return crypt_msg
+
+def send_msg(conn, msg, k):
+	conn.send(pickle.dumps(crypt(msg, k)))
+
+def recv_msg(conn, k):
+	msg=crypt(pickle.loads(conn.recv(1024)), k)
+	return msg
 
 HOST = '127.0.0.1'
-PORT = 8080
-
 sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
+#port = int(input('ВАШ ПОРТ:'))
+port=9090
+port=random_free_port(port)          
+sock.bind((HOST,port))
+print('Номер порта:',port)
+
+
+sock.listen(0)			
+sock.setblocking(1)
 conn, addr = sock.accept()
 
+b = randint(1, 256)
+
 msg = conn.recv(1024)
-print(pickle.loads(msg))
+p, g, Aa = pickle.loads(msg)
+
+Bb = chis(g,b,p)
+#print('B=',Bb)
+conn.send(pickle.dumps(Bb))
+
+K = chis(Aa,b,p)
+print('K=',K)
+while True:
+	try:
+		msg = recv_msg(conn, K)
+		print(msg)
+		send_msg(conn, 'Сервер получил и расшифровал', K)
+	except:
+		break
 
 conn.close()
